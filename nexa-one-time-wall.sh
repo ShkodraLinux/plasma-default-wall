0ed74c2ada71d7df043d8604b4ed87952ffffc19
komaru.yml <170887716+komaru-meow@users.noreply.github.com>
2025-03-28 18:32:06 +0100

    [plasma-default-wall] [nexa-one-time-wall] Fixup

--- nexa-one-time-wall.sh
+++ nexa-one-time-wall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set wallpaper
-qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var desktops = desktops(); for (var i = 0; i < desktops.length; i++) { var d = desktops[i]; d.wallpaperPlugin = 'org.kde.image'; d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General'); d.writeConfig('Image', 'file:///usr/share/wallpapers/Nexa Theme/Nexa Theme.png'); d.reloadConfig(); }"
+/usr/lib/qt6/bin/qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var desktops = desktops(); for (var i = 0; i < desktops.length; i++) { var d = desktops[i]; d.wallpaperPlugin = 'org.kde.image'; d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General'); d.writeConfig('Image', 'file:///usr/share/wallpapers/artwork/Nexa Theme/Nexa Theme.png'); d.reloadConfig(); }"
 
 # send nexa linux notification
 notify-send -i "/usr/share/pixmaps/nexalinux-logo.png" -a "Nexa Linux" "Welcome to Nexa Linux!" "We hope you will love using our distro."
